c39ad8a9f61d789303fa058539166be697c21676
Dave Shoreman <dshoreman@users.noreply.github.com>
2022-01-23 21:32:06 +0000

    sfd

--- src/backend/backend.c
+++ src/backend/backend.c
@@ -412,8 +412,7 @@ void paint_all_new(session_t *ps, struct managed_win *t, bool ignore_damage) {
 				assert(ps->o.blur_method != BLUR_METHOD_NONE);
 				region_t reg_noframe;
 				win_get_region_noframe_local(w, &reg_noframe);
-				pixman_region32_translate(&reg_noframe, w->g.x, w->g.y);
-				pixman_region32_subtract(&reg_noframe, &reg_noframe, &reg_visible);
+				pixman_region32_subtract(&reg_noframe, &reg_noframe, &reg_frame);
 
 				// FIXME Think more about combining blur w/ opacity
 				// FIXME Don't hardcode opacity
